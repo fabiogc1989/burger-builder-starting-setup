c7a9b2bdca05a0d1e5cb3c98d66297442cf75563
Fabio Coimbra <fabiogc1989@gmail.com>
2020-07-26 13:24:27 +0100

    Create Checkout and Contact data page + react-router package added

---
+++ src/containers/ContactData/ContactData.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class ContactData extends Component {
+  state = {
+      name: '',
+      email: '',
+      address: {
+          street: '',
+          postalCode: ''
+      }
+  };
+
+  render() {
+      return (
+          <div>
+              <h4>Enter your Contact Data</h4>
+              <form>
+                  <input type="text" name="name" placeholder="Your name" />
+                  <input type="email" name="email" placeholder="Your email" />
+                  <input type="text" name="street" placeholder="Your street" />
+                  <input type="text" name="email" placeholder="Your email" />
+              </form>
+          </div>
+      );
+  }
+}
+
+export default ContactData;
