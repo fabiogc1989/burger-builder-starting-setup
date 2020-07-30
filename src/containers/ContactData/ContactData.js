d3cb33f348b5a095c01bf5260eaff83223c03a57
Fabio Coimbra <fabiogc1989@gmail.com>
2020-07-30 13:01:12 +0100

    ContactData Component: Fix Price

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -22,7 +22,7 @@ class ContactData extends Component {
     this.setState({ loading: true });
     const order = {
       ingredients: this.props.ingredients,
-      price: this.state.totalPrice,
+      price: this.props.price,
       customer: {
         name: "Fabio Coimbra",
         address: {
