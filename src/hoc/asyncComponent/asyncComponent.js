2bd6bd123e0dd16b93114aa4fd3fa0734ada7448
Fabio Coimbra <fabiogc1989@gmail.com>
2020-09-04 15:57:30 +0100

    update npm packages and fix asyncComponent.js

--- src/hoc/asyncComponent/asyncComponent.js
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 const asyncComponent = (importComponent) => {
+    debugger;
     return class extends Component{
         state = {
             component: null
@@ -8,13 +9,14 @@ const asyncComponent = (importComponent) => {
 
         componentDidMount(){
             importComponent().then(cmp => {
-                this.setState({component: cmp});
+                debugger;
+                this.setState({component: cmp.default});
             })
         }
 
         render(){
             const C = this.state.component;
-
+            debugger;
             return C ? <C {...this.props} /> : null;
         }
     };
